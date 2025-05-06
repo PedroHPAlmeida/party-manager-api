2f5c22b6d08c501b6a9e4584d48b6201fff9f4ab
tlobato <tlobato@ciandt.com>
2025-05-06 19:37:53 -0300

    feat: add files to handle with exceptions

---
+++ src/main/java/com/partymanager/exception/exceptionDTO/ErrorMessageDTO.java
@@ -0,0 +1,16 @@
+package com.partymanager.exception.exceptionDTO;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private Integer status;
+    private String message;
+    private String path;
+    private LocalDateTime timestamp;
+}
