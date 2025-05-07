1e59cdf8727c4c69206d71127c870286717c38d6
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2025-05-06 21:23:30 -0300

    refact: remove unused method

--- src/main/java/com/partymanager/repositories/UserRepository.java
+++ src/main/java/com/partymanager/repositories/UserRepository.java
@@ -4,5 +4,4 @@ import com.partymanager.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
 }
